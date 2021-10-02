190f97d45d1c567c7933f045abd5307a2bc344f4
anadeso <anadeso90@gmail.com>
2021-10-02 17:23:52 -0300

    Implementing repository, service and using them in classses resource

---
+++ backend/src/main/java/com/devsuperior/dscliente/services/ClientService.java
@@ -0,0 +1,19 @@
+package com.devsuperior.dscliente.services;
+
+import com.devsuperior.dscliente.entities.Client;
+import com.devsuperior.dscliente.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public List<Client> findAll() {
+        return clientRepository.findAll();
+    }
+}

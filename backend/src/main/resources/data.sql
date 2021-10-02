7ae1022ed6b8d17123e66563a262bf6d0075a750
anadeso <anadeso90@gmail.com>
2021-10-02 18:40:35 -0300

    Database seeding

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Alícia Vitória Teixeira', '102.105.790-80', 6500, TIMESTAMP WITH TIME ZONE '1983-10-25T10:30:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Vicente Antonio Pinto', '258.197.350-13', 3000, TIMESTAMP WITH TIME ZONE '1970-09-20T10:30:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Vitória Mariana Nina', '827.466.320-98', 2000, TIMESTAMP WITH TIME ZONE '1976-05-25T10:30:00Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Sara Stella Yasmin Pinto', '864.391.690-09', 4000, TIMESTAMP WITH TIME ZONE '1981-02-24T10:30:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Sebastiana Elaine Brito', '180.694.680-76', 3000, TIMESTAMP WITH TIME ZONE '1980-01-25T10:30:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Leandro Benjamin Brito', '829.022.630-66', 3000, TIMESTAMP WITH TIME ZONE '1965-06-20T10:30:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Emilly Ayla Bárbara', '860.819.010-92', 6500, TIMESTAMP WITH TIME ZONE '1983-07-25T10:30:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Marli Jéssica Nascimento', '308.852.380-52', 6500, TIMESTAMP WITH TIME ZONE '1983-07-25T10:30:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Antonella Lorena', '126.882.540-90', 6500, TIMESTAMP WITH TIME ZONE '1983-07-25T10:30:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Joaquim Vitor Nascimento', '126.882.540-90', 6500, TIMESTAMP WITH TIME ZONE '1983-07-25T10:30:00Z', 2);
+

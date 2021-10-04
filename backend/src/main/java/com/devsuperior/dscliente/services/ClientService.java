93759d705742c0a0be3049ee96b59ad1aeaf94f5
anadeso <anadeso90@gmail.com>
2021-10-04 11:48:59 -0300

    Implementing search by id

--- backend/src/main/java/com/devsuperior/dscliente/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dscliente/services/ClientService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -27,4 +28,13 @@ public class ClientService {
                         .collect(Collectors.toList());
         return listDto;
     }
+
+    @Transactional(readOnly = true)
+    public ClientDto findById(Long id) {
+        Optional<Client> objEntity = clientRepository.findById(id);
+        Client entity = objEntity.get();
+        return new ClientDto(entity);
+    }
+
+
 }
